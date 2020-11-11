4cc36bb5acedd3f4c257f2011ae6ea2d62887f7a
Tobias Judd <tobias_judd@hotmail.com>
2020-11-11 15:51:22 -0600

    First iteration of Level 2

--- static/js/app.js
+++ static/js/app.js
@@ -11,6 +11,9 @@ var tbody = d3.select("tbody");
 
 var button = d3.select("#filter-btn");
 var searchField = d3.select("#datetime");
+var searchField1 = d3.select("#city");
+var searchField2 = d3.select("#city");
+var searchField3 = d3.select("#city");
 
 var resetbtn = d3.select("#reset-btn");
 
@@ -32,12 +35,32 @@ upload(data);
 button.on("click", () => {
     d3.event.preventDefault();
     var inputDate = searchField.property("value");
+    // Level 2 search of other variable - by city
+    var inputCity = searchField1.property("value").toLowerCase();
+    // Level 2 search of other variable - by state
+    var inputState = searchField2.property("value").toLowerCase();
+    // Level 2 search of other variable - by shape
+    var inputShape = searchField3.property("value").toLowerCase();
     console.log(inputDate);
+    console.log(inputCity);
+    console.log(inputState);
+    console.log(inputShape);
     var filterDate = data.filter(data => data.datetime === inputDate);
     console.log(filterDate);
+    // Level 2 search of other variable - by city
+    var filterCity = data.filter(data => data.city === inputCity);
+    console.log(filterCity);
+    // Level 2 search of other variable - by state
+    var filterState = data.filter(data => data.state === inputState);
+    console.log(filterState);
+    // Level 2 search of other variable - by shape
+    var filterShape = data.filter(data => data.shape === inputShape);
+    console.log(filterState);
     tbody.html("");
     upload(filterDate);
-
+    upload(filterCity);
+    upload(filterState);
+    upload(filterShape);
 })
 
 resetbtn.on("click", () => {
